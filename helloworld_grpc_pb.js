4df0192db5860270aff6678ee087c62a8d1ef94a
Max Base <MaxBaseCode@Gmail.Com>
2021-08-09 14:54:09 +0430

    Add files via upload

---
+++ helloworld_grpc_pb.js
@@ -0,0 +1,49 @@
+// GENERATED CODE -- DO NOT EDIT!
+
+// Original file comments:
+// protoc -I . --grpc_out=. --plugin=protoc-gen-grpc=grpc_cpp_plugin helloworld.proto
+// protoc -I . --cpp_out=. helloworld.proto
+'use strict';
+var grpc = require('@grpc/grpc-js');
+var helloworld_pb = require('./helloworld_pb.js');
+
+function serialize_helloworld_HelloReply(arg) {
+  if (!(arg instanceof helloworld_pb.HelloReply)) {
+    throw new Error('Expected argument of type helloworld.HelloReply');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_helloworld_HelloReply(buffer_arg) {
+  return helloworld_pb.HelloReply.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_helloworld_HelloRequest(arg) {
+  if (!(arg instanceof helloworld_pb.HelloRequest)) {
+    throw new Error('Expected argument of type helloworld.HelloRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_helloworld_HelloRequest(buffer_arg) {
+  return helloworld_pb.HelloRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+
+// The greeting service definition.
+var GreeterService = exports.GreeterService = {
+  // Sends a greeting
+sayHello: {
+    path: '/helloworld.Greeter/SayHello',
+    requestStream: false,
+    responseStream: false,
+    requestType: helloworld_pb.HelloRequest,
+    responseType: helloworld_pb.HelloReply,
+    requestSerialize: serialize_helloworld_HelloRequest,
+    requestDeserialize: deserialize_helloworld_HelloRequest,
+    responseSerialize: serialize_helloworld_HelloReply,
+    responseDeserialize: deserialize_helloworld_HelloReply,
+  },
+};
+
+exports.GreeterClient = grpc.makeGenericClientConstructor(GreeterService);
